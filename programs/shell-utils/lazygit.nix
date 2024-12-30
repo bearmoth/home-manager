4cec181c56d1b870acfb1d006582f39d396160af
Phil Davies <7753727+bearmoth@users.noreply.github.com>
2024-12-30 20:39:25 +1100

    feat(apps): add lazygit

---
+++ programs/shell-utils/lazygit.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+let
+	cfg = config.lazygit;
+in {
+	options.lazygit.enable = lib.mkEnableOption "lazygit (tui for git)";
+
+	config = lib.mkIf cfg.enable {
+		home.packages = with pkgs; [ lazygit ];
+	};
+}
